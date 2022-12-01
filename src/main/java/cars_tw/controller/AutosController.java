258a5442dcbc566ef0aab8e608a3291e845378b2
Jazzi24 <perezhernandezjazziel183@gmail.com>
2022-12-01 12:01:56 -0600

    Comentarios de controller

--- src/main/java/cars_tw/controller/AutosController.java
+++ src/main/java/cars_tw/controller/AutosController.java
@@ -1,7 +1,7 @@
 /**
  * Autor: Jazziel Pérez Hernández
  * Creación: 22/11/2022
- * Actualización: 30/11/2022
+ * Actualización: 01/12/2022
  * Descripción: Clase AutosController
  */
 package cars_tw.controller;
@@ -32,44 +32,70 @@ public class AutosController {
     private Autos autos;
     private List<Autos> listaRegistros;
 
+    /**
+     * método que inicializa los artibutos de la clase
+     */
     @PostConstruct
     public void init() {
         service = new AutosServiceImpl();
         autos = new Autos();
         listaRegistros = service.obtenerRegistros();
     }
-
+    /**
+     * método para insertar registro
+     */
     public void crearRegistro() {
         service.insertarRegistro(autos);
-        System.out.println("guardardado exitosamente");
+         listaRegistros = service.obtenerRegistros();
     }
-
+    /**
+     * 
+     * @param autos metodo set de autos
+     */
     public void setAutos(Autos autos) {
         this.autos = autos;
     }
 
+    /**
+     * 
+     * @return metodo get de autos
+     */
     public Autos getAutos() {
         return autos;
     }
 
+    /**
+     * 
+     * @return metodo get de listaRegistros
+     */
     public List<Autos> getListaRegistros() {
         return listaRegistros;
     }
 
+    /**
+     * 
+     * @param event método para actualizar registro.
+     */
     public void onRowEdit(RowEditEvent event) {
         autos = (Autos) event.getObject();
         service.actualizarRegistro(autos);
+        listaRegistros = service.obtenerRegistros();
         FacesMessage mensaje = 
                 new FacesMessage("Registro editado exitosamente");
         FacesContext.getCurrentInstance()
                 .addMessage(null, mensaje);
     }
-
+/**
+ * 
+ * @param autos método para eliminar registro
+ */
     public void eliminarRegistro(Autos autos) {
         service.eliminarRegistro(autos);
-        System.out.println("Eliminando exitosamente...");
+        listaRegistros = service.obtenerRegistros();
     }
-
+    /**
+     * Metodo para eliminar el contenido del objeto autos
+     */
      public void limpiar(){
         this.autos= new Autos();
     }
