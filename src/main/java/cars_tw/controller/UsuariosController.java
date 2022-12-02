efeabd73469b7e253fd05f7b488a71df3fed04ca
medina Gris <medina Gris@Gris>
2022-12-01 22:36:36 -0600

    comentarios a vista

--- src/main/java/cars_tw/controller/UsuariosController.java
+++ src/main/java/cars_tw/controller/UsuariosController.java
@@ -1,7 +1,7 @@
 /**
  * Autor: Jazziel Pérez Hernández
  * Creación: 22/11/2022
- * Actualización: 30/11/2022
+ * Actualización: 01/12/2022
  * Descripción: Clase UsuariosController
  */
 package cars_tw.controller;
@@ -19,18 +19,24 @@ import javax.faces.context.FacesContext;
 import org.primefaces.event.RowEditEvent;
 
 /**
- * Definición se nombra en bean como user para instanciarlos y poder llamar a 
+ * Definición se nombra en bean como user para instanciarlos y poder llamar a
  * los métodos getters y setter al igual que controlar el ciclo de vida
  */
 @RequestScoped
 @ManagedBean(name = "user")
 public class UsuariosController implements Serializable {
 
-    
+    /**
+     * Se declaran 2 objetos de IUsuariosService y Usuarios al igual que una
+     * lista de objetos Usuarios
+     */
     private IUsuariosService service;
     private Usuarios usuarios;
     private List<Usuarios> listaRegistros;
 
+    /**
+     *método que inicializa los artibutos de la clase
+     */
     @PostConstruct
     public void init() {
         service = new UsuariosServiceImpl();
@@ -38,44 +44,65 @@ public class UsuariosController implements Serializable {
         listaRegistros = service.obtenerRegistros();
     }
 
+    /**
+     *método para insertar registro
+     */
     public void crearRegistro() {
         service.insertarRegistro(usuarios);
         listaRegistros = service.obtenerRegistros();
     }
 
+    /**
+     *
+     * @return metodo get de usuarios
+     */
     public Usuarios getUsuarios() {
         return usuarios;
     }
 
+    /**
+     *
+     * @param usuarios metodo set de usuarios
+     */
     public void setUsuarios(Usuarios usuarios) {
         this.usuarios = usuarios;
     }
 
+    /**
+     *
+     * @return metodo get de listaRegistros
+     */
     public List<Usuarios> getListaRegistros() {
         return listaRegistros;
     }
 
+    /**
+     *
+     * @param event método para actualizar registro.
+     */
     public void onRowEdit(RowEditEvent event) {
-
         usuarios = (Usuarios) event.getObject();
-        //System.out.println(usuarios.getIdUser());
-        //System.out.println(usuarios.getNombre());
-
         service.actualizarRegistro(usuarios);
         listaRegistros = service.obtenerRegistros();
-        FacesMessage mensaje = 
-                new FacesMessage("Registro editado exitosamente");
+        FacesMessage mensaje
+                = new FacesMessage("Registro editado exitosamente");
         FacesContext.getCurrentInstance().
                 addMessage(null, mensaje);
     }
 
+    /**
+     *
+     * @param usuarios método para eliminar registro
+     */
     public void eliminarRegistro(Usuarios usuarios) {
         service.eliminarRegistro(usuarios);
         listaRegistros = service.obtenerRegistros();
-        System.out.println("Eliminando...");
     }
 
-    public void limpiar(){
-        this.usuarios= new Usuarios();
+    /**
+     *Metodo para eliminar el contenido del objeto autos
+     */
+    public void limpiar() {
+        this.usuarios = new Usuarios();
     }
 }
